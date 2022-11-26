5b4711dc0bc590987b272955b4657f699f65f1c3
vazand <vazandvel@gmail.com>
2022-11-26 15:02:06 +0530

    boxed quote

--- src/main/scala/io/github/QuotesCollection.scala
+++ src/main/scala/io/github/QuotesCollection.scala
@@ -1,5 +1,6 @@
 package io.github.vazand.quotes
 trait QuotesCollection {
+
   val luffy_quote_1 = "If I give up now, I'm going to regret it"
   val luffy_quote_2 = "If you don’t take risks, you can’t create a future."
   val naruto_1 = "If you don’t like your destiny, don’t accept it."
@@ -13,9 +14,10 @@ trait QuotesCollection {
     "Every journey begins with a single step. We just have to have patience."
   val mighty_guy =
     "It’s not always possible to do what we want to do, but it’s important to believe in something before you actually do it"
-  
-  val gilgamesh_fate_zero = "Do exactly as you like. That is the true meaning of pleasure. Pleasure leads to joy and joy leads to happiness."
-  
+
+  val gilgamesh_fate_zero =
+    "Do exactly as you like. That is the true meaning of pleasure. Pleasure leads to joy and joy leads to happiness."
+
   val list_of_quotes: List[String] =
     List(
       luffy_quote_1,
@@ -29,5 +31,47 @@ trait QuotesCollection {
       mighty_guy,
       gilgamesh_fate_zero
     )
+
+  /** This method will return a random String from a list
+    *
+    * @param list
+    *   is list of Strings
+    * @return
+    *   String
+    */
+
+  def randomQuote(list: List[String]): String = {
+    val rand = new scala.util.Random
+    val random_str = rand.nextInt(list.length)
+    return list(random_str)
+  }
+
+  val quote = randomQuote(list_of_quotes)
+
+  /** This Function returns Boxed String
+    *
+    * example
+    * {{{
+    * val str = "Hello"
+    * returns like this
+    *   +-------+
+    *   | Hello |
+    *   +-------+
+    * }}}
+    *
+    * @param str
+    * @return
+    *   boxed String
+    */
+  def boxed_quote(str: String): String = {
+    val str_len = str.length() + 2
+    val char: Char = '-'
+    val box_up_down = "+" + (char.toString() * str_len).concat("+")
+    val box_center = "| " + str + " |"
+    return "\n" + box_up_down + "\n" + box_center + "\n" + box_up_down + "\n"
+
+  }
+  val quote_boxed = (boxed_quote(quote))
+  println(quote_boxed)
 
 }
--- src/main/scala/io/github/RandomQuotes.scala
+++ src/main/scala/io/github/RandomQuotes.scala
@@ -1,18 +1,5 @@
 package io.github.vazand.quotes
-object RandomQuotes extends App with QuotesCollection{
-  println("   Hi   ")
+object RandomQuotes extends App with QuotesCollection {
+  println(" Hello !  ")
 
-  val rand = new  scala.util.Random
-
-  def randomQuote(list:List[String]):String ={
-    val random_str = rand.nextInt(list.length)
-    return list(random_str)
-  }
-  
-  def sayHello(str:String):String = {
-    return "Hello "+str
-  }
-  
-  //println(randomQuote(list_of_quotes))
-  
 }
